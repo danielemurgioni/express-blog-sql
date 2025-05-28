f8a53ac3f8f1b964372db3c9b301cf8264ee03e2
danielemurgioni <dashomew@gmail.com>
2025-05-28 17:25:45 +0200

    [UPD] recover specific posts with SHOW from the database through method query()

--- controllers/postController.js
+++ controllers/postController.js
@@ -21,8 +21,26 @@ const index = (req, res) => {
 
 //metodo show
 const show = (req, res) => {
+
+    //recupero l'id
     const id = req.params.id
-    res.send(`Post con id: ${id}`)
+
+    //variabile querySQL per recuperare i post con un id segnaposto
+    const querySQL = `SELECT * FROM db_blog.posts WHERE id = ?`
+
+    //eseguo la querySQL con l'utilizzo del metodo "query()"
+    connection.query(querySQL, [id], (err, result) => {
+        if (err) { //messaggio di errore se la querySQL è sbagliata
+            res.status(500).json({ error: "Database query failed" })
+        }
+        if (result.length === 0) { //messaggio di errore se il post non viene trovato
+            res.status(404).json({ error: "Post not found" })
+        }
+        else {
+            console.log(result)
+            res.json(result)
+        }
+    })
 }
 
 module.exports = { index, show }
