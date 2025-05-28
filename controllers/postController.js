8c5b4a58554f5aafa131f29cece0347d30b65e93
danielemurgioni <dashomew@gmail.com>
2025-05-28 17:14:05 +0200

    [UPD] recover posts data with the index from the database through method query()

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,7 +1,22 @@
+//recupero la variabile di connessione al database
+const connection = require("../data/db")
 
 //metodo index
 const index = (req, res) => {
-    res.send("Elenco posts")
+
+    //variabile querySQL per recuperare i post dal database
+    const querySQL = `SELECT * FROM db_blog.posts`
+
+    //eseguo la querySQL con l'utilizzo del metodo "query()"
+    connection.query(querySQL, (err, result) => {
+        if (err) {
+            res.status(500).json({ error: "Database query failed" })
+        }
+        else {
+            console.log(result)
+            res.json(result)
+        }
+    })
 }
 
 //metodo show
--- data/db.js
+++ data/db.js
@@ -5,13 +5,19 @@ const mysql = require("mysql2")
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "12345678",
     database: "db_blog"
 })
 
 //utilizzo la variabile con l'uso del metodo "connect"
-connection.connect(() => {
-    console.log(`connected to MySQL`)
+connection.connect((err) => {
+    //verifico se c'è un errore
+    if (err) {
+        console.log(err)
+    }
+    else {
+        console.log(`connected to MySQL`)
+    }
 })
 
 module.exports = connection;
